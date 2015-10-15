c5a19515efa43c30a9a3a6b41308e22b080eaa7b
henry <henryz_e@163.com>
2015-10-15 19:50:16 +0800

    20151015

---
+++ obj1/oslab/linux-0.11/include/pthread.h
@@ -0,0 +1,29 @@
+#ifndef _PTHREAD_H
+#define _PTHREAD_H
+
+#define PTHREAD_CREATE_JOINABLE 0
+#define PTHREAD_CREATE_DETACHED 1
+
+//move to kernel in future
+#define PTHREAD_CANCEL_DISABLE  0
+#define PTHREAD_CANCEL_ENABLE   1
+
+typedef struct _pthread_attr_t_{
+	int cancel_state;
+	int detached;
+}pthread_attr_t;
+
+typedef int pthread_t;
+
+int pthread_attr_init(pthread_attr_t *attr);
+int pthread_attr_setdetachstate(pthread_attr_t *attr, int detachstate);
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                          void *(*start_routine) (void *), void *arg);
+void pthread_exit(void *retval);
+int pthread_cancel(pthread_t thread);
+int pthread_setcancelstate(int state, int *oldstate);
+void pthread_cleanup_push(void (*routine)(void *), void *arg);
+void pthread_cleanup_pop(int execute);
+void pthread_testcancel(void);
+
+#endif /* _PTHREAD_H */
---
+++ obj1/oslab/linux-0.11/lib/pthread.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <pthread.h>
+
+//move to kernel in future
+#define PTHREAD_CANSIG_NOTYET   0
+#define PTHREAD_CANSIG_CATCHED  1
+
+
+int pthread_attr_init(pthread_attr_t *attr)
+{
+	//move to kernel in future
+	attr->cancel_state = PTHREAD_CANCEL_DISABLE;
+	attr->cancel_sign = PTHREAD_CANSIG_NOTYET;
+
+	attr->detached = PTHREAD_CREATE_JOINABLE;
+	
+	return (0); 
+}
+
+int pthread_attr_setdetachstate(pthread_attr_t *attr, int detachstate)
+{
+	attr->detached = detachstate;
+
+	return (0); 
+}
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                          void *(*start_routine) (void *), void *arg)
+{
+
+}
+
+void pthread_exit(void *retval)
+{
+
+}
+
+
+int pthread_cancel(pthread_t thread)
+{
+	
+}
+
+int pthread_setcancelstate(int state, int *oldstate)
+{
+	*oldstate = attr->cancel_state;
+	attr->cancel_state = state;
+
+	return 0;
+}
+
+void pthread_cleanup_push(void (*routine)(void *), void *arg)
+{
+
+}
+
+void pthread_cleanup_pop(int execute)
+{
+
+}
+
+void pthread_testcancel(void)
+{
+	if(attr->cancel_state == PTHREAD_CANCEL_ENABLE){
+		if(attr->cancel_sign == PTHREAD_CANSIG_CATCHED){
+			pthread_exit(NULL);
+		}
+	}
+}
+
+
