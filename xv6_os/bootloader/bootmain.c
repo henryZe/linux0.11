632ff93236e6eac2c7974700921bf0af2dec3e21
henry.zeng <henry.zeng@alitech.com>
2016-03-08 18:42:44 +0800

    bootasm.S is done

---
+++ xv6_os/bootloader/bootmain.c
@@ -0,0 +1,31 @@
+/*	Boot loader.
+
+	Part of the boot sector, along with bootasm.S, which calls bootmain().
+	bootasm.S has put the processor into protected 32-bit mode.
+	bootmain() loads an ELF kernel image from the disk starting at
+	sector 1 and then jumps to the kernel entry routine.
+*/
+
+#include <types.h>
+#include <elf.h>
+#include <x86.h>
+#include <memlayout.h>
+
+#define SECTSIZE	512
+
+void readseg(uchar *, uint, uint);
+
+void bootmain(void)
+{
+	struct elfhdr *elf;
+
+
+
+
+}
+
+
+
+
+
+
